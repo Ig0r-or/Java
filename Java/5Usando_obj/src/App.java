ac14831e1ca0fe3aa077c24aa880faaab6a21e79
Igor <164401980+Ig0r-or@users.noreply.github.com>
2026-03-03 20:11:27 +0000

    Atualizações

--- Java/5Usando_obj/src/App.java
+++ Java/5Usando_obj/src/App.java
@@ -1,7 +1,19 @@
 public class App{
 
     public static void main(String[] args) throws Exception{
-        Paciente paciente1 = new Paciente();
+
+        Paciente p1 = new Paciente(
+            1,
+            "Neymar",
+            "05/02/1992",
+            "11409624716",
+            ""
+        );
+
+        p1.Apresentar();
+
+        p1.atualizarContado();
+
     }
 } 
 
