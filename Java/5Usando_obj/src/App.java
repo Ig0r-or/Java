ac48ca63ca7aef8e60bced9cbec6fb224c318d5b
Igor <164401980+Ig0r-or@users.noreply.github.com>
2026-03-09 17:40:23 +0000

    Remove instantiation of Usuario and Emprestimo classes; add list of livros to display available titles

--- Java/5Usando_obj/src/App.java
+++ Java/5Usando_obj/src/App.java
@@ -1,14 +1,16 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class App{
 
     public static void main(String[] args) throws Exception{
-        Usuario u = new Usuario(1, "Igor", "20", "11/11/2005" );
-        Livro l1 = new Livro(2, "Neymar Historia", "Craque Neto", "disponível");
+        List<Livro> listaDeLivros = new ArrayList<>();
 
-        Emprestimo em = new Emprestimo(4, "10:30", "05/02/2025", "Em andamento", l1, u);
-        System.out.println(
-            "Emprestimo de " + em.getUsuario().getNome() + " do livro " + em.getLivro().getTitulo() + " escrito por " + em.getLivro().getAutor()
-        );
-    
+        listaDeLivros.add(new Livro(01, "O Principe que não se tornou Rei!", "By: Alamo", "Disponivel"));
+        
+        for(Livro l : listaDeLivros){
+            System.out.println(l);
+        }
     }
 } 
 
