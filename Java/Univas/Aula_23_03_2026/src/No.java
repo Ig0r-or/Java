bbe766c6be3bb13f30dae28f853bd1d95387aed5
Igor <164401980+Ig0r-or@users.noreply.github.com>
2026-03-24 00:18:06 +0000

    Adicionar classes EstruturaLinear e No, e implementar método de inserção; criar arquivos de configuração e README

---
+++ Java/Univas/Aula_23_03_2026/src/No.java
@@ -0,0 +1,11 @@
+package Java.Univas.Aula_23_03_2026.src;
+
+public class No{
+    int dado;
+    No proximo;
+
+    public No(int dado){
+        this.dado = dado;
+        this.proximo = null;
+    }
+}
